c827c4df1bb0859ddfee5d5f86adce82b185f2bc
Jason <jason.ou@nyu.edu>
2019-01-03 22:52:39 -0500

    altering infrastructure with cookies

--- src/server/controllers/connections/connectionController.js
+++ src/server/controllers/connections/connectionController.js
@@ -1,12 +1,20 @@
+const uuidv4 = require('uuid/v4');
+
 const playerController = require('../players/playerController.js');
 
 let connectionController = {};
 connectionController.connections = [];
 connectionController.establishSSE = establishSSE;
+connectionController.setCookie = setCookie;
 
 function establishSSE (req, res, next) {
+  console.log('----- connectionController.establishSSE called. -----');
   res.currentId = 1;
-  connectionController.connections.push(res);
+  connectionController.connections.push({
+    id : req.cookies['chessmates'],
+    name : null,
+    sse : res,
+  });
 
   res.header(200);
   res.header('Content-Type', 'text/event-stream');
@@ -18,6 +26,15 @@ function establishSSE (req, res, next) {
 
 function requestHeartbeat (req, res, next) {
   
+}
+
+function setCookie (req, res, next) {
+  console.log('----- connectionController.setCookie called. -----');
+  if(!req.cookies['chessmates']){
+    const uuid = uuidv4();
+    res.cookie('chessmates', uuid, {expire : new Date() + 9999});
+    next();
+  }
 }
 
 module.exports = connectionController;
--- src/server/server.js
+++ src/server/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(express.static('dist'));
+app.use(cookieParser());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -18,6 +19,9 @@ const connectionRouter = express.Router();
 const playerController = require('./controllers/players/playerController');
 const connectionController = require('./controllers/connections/connectionController.js');
 const sqlController = require('./controllers/sql/sqlController.js');
+
+app.use(express.static('dist'));
+app.use(connectionController.setCookie);
 
 app.get('/', (req, res, next) => {
   res.header(200);
